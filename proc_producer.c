44ad2580b38505c913d90e20497e6e983d0c0424
Roberto Rojas Segnini <roberto.t.x@hotmail.com>
2019-05-18 14:38:33 -0600

    struct processInfo added

--- proc_producer.c
+++ proc_producer.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+enum AlgorithmType{BEST=1, FIRST=2, WORST=3};
+enum ProcessState{RUNNING, BLOCKED, IN_CRITICAL_REGION};
+
+
+/* info used by our threadsx */
+struct processInfo {
+
+    pthread_t     PID;         /* ID returned by pthread_create() */
+    int           base_register;     /* Stores the process' starting memory address */
+    int           size;        /* The size (in lines) of the process */
+    int           execution_time; /* Amount of time the process is going to sleep */
+    enum ProcessState state;   /* The current state of the process */
+};
+
+void* printProcessState(void* pInfo){
+  struct processInfo *args = (struct processInfo *)pInfo;
+  printf("I am going to sleep now \n");
+  for (int i = 0; i<args->execution_time;i++){
+    printf("%d",args->size);
+    fflush(stdout);
+    sleep(1);
+  }
+  printf("I'm wide awake now. Bye \n");
+  //pthread_exit((void *)0);
+  //sleep(args->execution_time);
+}
 
 
 int main()
 {
-    enum AlgorithmType{BEST=1, FIRST=2, WORST=3};
-    int proc_size, exec_time,producer_wait;
+
+    int proc_size, exec_time, producer_wait;
+
+
 
     enum AlgorithmType algorithmNumber;
     printf(" 1) Best-fit \n 2) First-fit \n 3) Worst-fit \n");
@@ -16,9 +50,15 @@ int main()
     while (1){
       proc_size = rand() % 10 + 1;
       exec_time = (rand() % (60 - 20 + 1)) + 20; //num = (rand() % (upper – lower + 1)) + lower
+
+      pthread_t PID;
+      struct processInfo pinfo = {PID, 0, proc_size, exec_time, BLOCKED};
+      pthread_create(&PID, NULL, printProcessState, &pinfo);
       printf("create_thread with %d lines and %d s of execution time\n", proc_size, exec_time);
+      pthread_join(PID, NULL);
 
       producer_wait = (rand() % (60 - 30 + 1)) + 30;
+      //sleep(1);//
       sleep(producer_wait);
 
     }
@@ -26,5 +66,8 @@ int main()
     return 0;
 }
 
+
+
 //Bibliography
 //https://www.geeksforgeeks.org/generating-random-number-range-c/
+//http://users.cs.cf.ac.uk/Dave.Marshall/C/node27.html
